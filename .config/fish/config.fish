bebe8a993d9bd28ec26302f7603432eb68b63ec9
Carlos Oliveira <cadu.oliveira@yahoo.com.br>
2024-04-24 15:32:01 -0300

    config fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,19 @@
+set fish_greeting ""
+set -gx TERM xterm-256color
+
+command -qv nvim && alias vim nvim
+
+alias dotfiles='/opt/homebrew/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+
 source ~/.aliases
 
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
+set -gx EDITOR nvim
 
-eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+switch (uname)
+    case Darwin
+        source (dirname (status --current-filename))/config-osx.fish
+    case Linux
+        source (dirname (status --current-filename))/config-linux.fish
+    case '*'
+        source (dirname (status --current-filename))/config-windows.fish
+end
